dfe816273496e32e8094d4e5d1783a9d97746fa2
eMoushen <99791335+eMoushen@users.noreply.github.com>
2022-03-16 23:07:04 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,6 +7,6 @@ int main()
     cout << "My name is Dmitry" << endl;
     cout << "I am 20 years old" << endl;
     cout << "I live in Stavropol" << endl;
-
+    cout << "Bye" << endl;
     return 0;
 }
