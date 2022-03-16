3b4c2ad058ffee80f90620e6d3e0e08ac2c36439
eMoushen <99791335+eMoushen@users.noreply.github.com>
2022-03-16 23:07:55 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, world!" << endl;
+ 
     cout << "My name is Dmitry" << endl;
     cout << "I am 20 years old" << endl;
     cout << "I live in Stavropol" << endl;
